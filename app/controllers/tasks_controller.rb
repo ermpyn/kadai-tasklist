af5c0cf0b70512f56499fe8d2c80e04e17134805
takuya.morie <erm.pyn@gmail.com>
2021-07-12 06:42:10 +0000

    07121542

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,9 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-      @tasks = Task.all
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
@@ -23,7 +22,7 @@ class TasksController < ApplicationController
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      render :index
     end
   end
 
@@ -49,10 +48,6 @@ class TasksController < ApplicationController
     private
 
   # Strong 
-    def set_task
-    @task = Task.find(params[:id])
-    end
-  
   def task_params
     params.require(:task).permit(:content, :status)
   end
